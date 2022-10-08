ee556e66788ee9201bd49adc36def869908b0467
Prikshit-anthal <prikshit_12013102@nitkkr.ac.in>
2022-10-08 12:18:05 +0530

    Day 2

---
+++ Maths/1.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// I really took time in it  LOLZZZZZZ
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+
+        cout << a << " " << c << " " << c << endl;
+    }
+    return 0;
+}
